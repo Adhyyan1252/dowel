e375b18578ad69b89652895e6d0626c9bcfe8fff
Adhyyan1252 <adhyyan1252@gmail.com>
2020-05-21 19:12:56 +0530

    Added support for dynamic fields in CSV Output
    Before this commit, adding a new key in the tabular type
    after the first call would lead to a warning.
    
    Now, if a new key is added after the initial call
    CSV_output will rewrite the CSV file to include
    additional columns corresponding to the new keys.
    
    This allows for dynamic keys when using Tabulars.
    
    Resolves #45

--- src/dowel/csv_output.py
+++ src/dowel/csv_output.py
@@ -14,7 +14,8 @@ class CsvOutput(FileOutput):
     """
 
     def __init__(self, file_name):
-        super().__init__(file_name)
+        super().__init__(file_name,
+                         mode='w+')  # opens a new file for reading and writing
         self._writer = None
         self._fieldnames = None
         self._warned_once = set()
@@ -28,33 +29,72 @@ class CsvOutput(FileOutput):
     def record(self, data, prefix=''):
         """Log tabular data to CSV."""
         if isinstance(data, TabularInput):
-            to_csv = data.as_primitive_dict
+            datadict = data.as_primitive_dict
 
-            if not to_csv.keys() and not self._writer:
+            if not datadict.keys() and not self._writer:
                 return
 
+            # If its the first call to record, then write header
             if not self._writer:
-                self._fieldnames = set(to_csv.keys())
-                self._writer = csv.DictWriter(
-                    self._log_file,
-                    fieldnames=self._fieldnames,
-                    extrasaction='ignore')
+                self._fieldnames = set(datadict.keys())
+                self._writer = csv.DictWriter(self._log_file,
+                                              fieldnames=self._fieldnames,
+                                              extrasaction='ignore')
                 self._writer.writeheader()
 
-            if to_csv.keys() != self._fieldnames:
-                self._warn('Inconsistent TabularInput keys detected. '
-                           'CsvOutput keys: {}. '
-                           'TabularInput keys: {}. '
-                           'Did you change key sets after your first '
-                           'logger.log(TabularInput)?'.format(
-                               set(self._fieldnames), set(to_csv.keys())))
+            # If new keys are added, then add extra columns to the csv file
+            if not self._fieldnames.issuperset(datadict.keys()):
+                self.rewriteCSV(datadict.keys())
 
-            self._writer.writerow(to_csv)
+            # If any key is not present in the latest row,
+            # add a blank key-value pair
+            for key in self._fieldnames:
+                if key not in datadict:
+                    datadict[key] = ''
+            self._writer.writerow(datadict)
 
-            for k in to_csv.keys():
+            for k in datadict.keys():
                 data.mark(k)
         else:
             raise ValueError('Unacceptable type.')
+
+    def rewriteCSV(self, fieldnames):
+        """Rewrite the CSV file to accommodate the extra fieldnames.
+
+        This function will read the file, store the data,
+        and rewrite it back to the file after adding the
+        additional fields to all the entries.
+        The value of this column will be blank in these cells
+
+        :param fieldnames: The new set of fieldnames
+        """
+        # read data from file into a list
+        self._log_file.seek(0)
+        csv_reader = csv.DictReader(self._log_file)
+        csvdata = [row for row in csv_reader]
+
+        # clearing the file
+        self._log_file.truncate(0)
+        self._log_file.seek(0)
+
+        # output data back to the file with extra fields
+        for key in fieldnames:
+            if key not in self._fieldnames:
+                self._fieldnames.add(key)
+
+        self._writer = csv.DictWriter(self._log_file,
+                                      fieldnames=self._fieldnames,
+                                      extrasaction='ignore')
+
+        self._writer.writeheader()
+        for row in csvdata:
+            # There are keys which are not set in the current tabular record
+            # Setting them to blank
+            for curfield in self._fieldnames:
+                if curfield not in row:
+                    row[curfield] = ''
+
+            self._writer.writerow(row)
 
     def _warn(self, msg):
         """Warns the user using warnings.warn.
@@ -63,8 +103,9 @@ class CsvOutput(FileOutput):
         is the one printed.
         """
         if not self._disable_warnings and msg not in self._warned_once:
-            warnings.warn(
-                colorize(msg, 'yellow'), CsvOutputWarning, stacklevel=3)
+            warnings.warn(colorize(msg, 'yellow'),
+                          CsvOutputWarning,
+                          stacklevel=3)
         self._warned_once.add(msg)
         return msg
 
